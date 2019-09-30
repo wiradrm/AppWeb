838ed211b326d7b343bbdbb8f42435cd571b7a5b
Wira Darma <wirad24@gmail.com>
2019-09-30 18:42:44 +0800

    Second Commit

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,9 @@
     <title>Master</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href=" url('css/style.css')">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="url('makna.ico')" type="image/x-icon">
   </head>
   <body>
     <div class="wrapper">
